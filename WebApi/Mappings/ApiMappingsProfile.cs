0a2bf0b349ef9c0c195578b3bef0e85fd667b289
Lera <leraiztlt@gmail.com>
2020-03-20 11:51:16 +0300

    Добавлен WebApi

---
+++ WebApi/Mappings/ApiMappingsProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BusinessLogic.Services.Contracts.Models;
+using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Mappings
+{
+    public class ApiMappingsProfile : Profile
+    {
+        public ApiMappingsProfile ()
+        {
+            CreateMap<CategoryModel, CategoryDto>();  
+        }
+    }
+}
---
+++ WebApi/Models/CategoryModel.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class CategoryModel
+    {
+        /// <summary>
+        /// id категории
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название категории
+        /// </summary>
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// ключ для связи с родительской категорией
+        /// </summary>
+        public int? ParentCategoryId { get; set; }
+
+        /// <summary>
+        /// Родительская категория
+        /// </summary>
+        public virtual Category ParentCategory { get; set; }
+
+        /// <summary>
+        /// Подкатегории родительской категории
+        /// </summary>
+        public virtual ICollection<Category> Childs { get; set; }
+    }
+}
