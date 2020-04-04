ed985cc03d2e5033adc49b7750feba56cb4bd025
Konstantin Myrsaitov <myrsaitov@gmail.com>
2020-04-04 20:43:23 +0300

    Added class BusinessLogic.Services.Validators.CommentDtoValidator

---
+++ BLL/BusinessLogic.Services/Validators/CommentDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLogic.Services.Contracts.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Services.Validators
+{
+    public class CommentDtoValidator : AbstractValidator<CommentDto>
+    {
+        public CommentDtoValidator()
+        {
+            RuleFor(x => x.Body).NotEmpty().WithMessage("CommentDto поле Body не заполнено");
+        }
+    }
+}
