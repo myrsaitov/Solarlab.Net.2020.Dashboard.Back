e3ec0625275d31cac4945dc17d0894da839ef5d7
Konstantin Myrsaitov <myrsaitov@gmail.com>
2020-04-04 19:04:19 +0300

    Added class BusinesLogic.Services.Abstractions.IAdvertisementService

--- BLL/BusinesLogic.Services.Abstractions/IAdvertisementService.cs
+++ BLL/BusinesLogic.Services.Abstractions/IAdvertisementService.cs
@@ -1,13 +1,11 @@
-﻿using Academy.Business.Contracts;
-using Academy.Business.Contracts.Models;
-using Academy.DataAccess.Entities;
-using System;
+﻿using BusinessLogic.Services.Contracts;
+using BusinessLogic.Services.Contracts.Models;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinesLogic.Services.Abstractions
 {
+
     /// <summary>
     /// Интерфейс сервиса работы с объявлениями
     /// </summary>
@@ -19,5 +17,40 @@ namespace BusinesLogic.Services.Abstractions
         /// <returns>OperationResult</returns>
         Task<OperationResult<ICollection<AdvertisementDto>>> GetPaged(int? categoryId, int page, int pageSize);
 
+        /// <summary>
+        /// Получение объявления по идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор существующего объявления</param>
+        /// <returns>OperationResult</returns>
+        Task<OperationResult<AdvertisementDto>> GetById(int id);
+
+        /// <summary>
+        /// Создание объявления
+        /// </summary>
+        /// <param name="advertisementDto">Объявление. Транспортный объект.</param>
+        /// <returns>OperationResult</returns>
+        Task<OperationResult<bool>> Create(AdvertisementDto advertisementDto);
+
+        /// <summary>
+        /// Изменение объявления
+        /// </summary>
+        /// <param name="advertisementDto">Объявление. Транспортный объект.</param>
+        /// <returns>OperationResult</returns>
+        Task<OperationResult<bool>> Update(AdvertisementDto advertisementDto);
+
+        /// <summary>
+        /// Удаление объявления
+        /// </summary>
+        /// <param name="id">Идентификатор существующего объявления</param>
+        /// <returns>OperationResult</returns>
+        Task<OperationResult<bool>> Delete(int id);
+
+        /// <summary>
+        /// Добавить комментарий к объявлению
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="commentDto"></param>
+        /// <returns>OperationResult</returns>
+        Task<OperationResult<bool>> AddComment(int id, CommentDto commentDto);
     }
 }
