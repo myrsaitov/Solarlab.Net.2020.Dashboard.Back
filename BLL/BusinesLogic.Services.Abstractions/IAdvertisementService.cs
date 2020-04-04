51b60adbd37dc05154a70f0b6d5ce21806cdce9a
Konstantin Myrsaitov <myrsaitov@gmail.com>
2020-04-04 18:54:41 +0300

    Added class BusinessLogic.Services.Contracts.Models. AdvertisementDto

--- BLL/BusinesLogic.Services.Abstractions/IAdvertisementService.cs
+++ BLL/BusinesLogic.Services.Abstractions/IAdvertisementService.cs
@@ -1,8 +1,23 @@
-﻿using System;
+﻿using Academy.Business.Contracts;
+using Academy.Business.Contracts.Models;
+using Academy.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BusinesLogic.Services.Abstractions
 {
+    /// <summary>
+    /// Интерфейс сервиса работы с объявлениями
+    /// </summary>
     public interface IAdvertisementService
     {
+        /// <summary>
+        /// Получение списка всех объявлений
+        /// </summary>
+        /// <returns>OperationResult</returns>
+        Task<OperationResult<ICollection<AdvertisementDto>>> GetPaged(int? categoryId, int page, int pageSize);
+
     }
 }
