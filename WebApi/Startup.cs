98b68ca6f9655f6407ddbdd3832e48041cd4af79
Konstantin Myrsaitov <myrsaitov@gmail.com>
2020-04-05 13:01:43 +0300

    Done!

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using BusinesLogic.Services.Abstractions;
+using BusinessLogic.Services.Abstractions;
 using BusinessLogic.Services;
 using BusinessLogic.Services.Abstractions;
 using BusinessLogic.Services.Mapping;
