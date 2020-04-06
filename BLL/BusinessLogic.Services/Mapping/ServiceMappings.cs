eff5cd722ba2868d2bc2c5f1fae55149561181c8
Konstantin Myrsaitov <myrsaitov@gmail.com>
2020-04-06 17:31:53 +0300

    Запустился swagger, но с ошибкой

--- BLL/BusinessLogic.Services/Mapping/ServiceMappings.cs
+++ BLL/BusinessLogic.Services/Mapping/ServiceMappings.cs
@@ -14,29 +14,55 @@ namespace BusinessLogic.Services.Mapping
     {
         public ServiceMappings()
         {
+
+
+
             CreateMap<Tag, TagDto>();
             CreateMap<Category, CategoryDto>()
                 .ForMember(d => d.Childs, opt => opt.Ignore());
-            //CreateMap<Comment, CommentDto>();
-
+            CreateMap<Comment, CommentDto>();
             CreateMap<Advertisement, AdvertisementDto>()
-                .ForMember(d => d.Tags, opt => opt.Ignore());
-
-           // CreateMap<CommentDto, Comment>()
-                //.ForMember(d => d.CommentDate, opt => opt.Ignore());
+                 .ForMember(d => d.Tags, opt => opt.Ignore());
+            CreateMap<CommentDto, Comment>()
+                .ForMember(d => d.ParentAdvertisement, opt => opt.Ignore())
+                .ForMember(d => d.CommentDate, opt => opt.Ignore());
             CreateMap<AdvertisementDto, Advertisement>()
                 .ForMember(d => d.Id, opt => opt.Ignore())
                 .ForMember(d => d.Tags, opt => opt.Ignore())
-                //.ForMember(d => d.Comments, opt => opt.Ignore())
+                .ForMember(d => d.Comments, opt => opt.Ignore())
                 .ForMember(d => d.Category, opt => opt.Ignore());
-
-            
             CreateMap<CategoryDto, Category>()
                 .ForMember(d => d.Childs, opt => opt.Ignore());
 
-            CreateMap<CategoryCreateDto, Category>()
-            .ForMember(d => d.ParentCategory, map => map.Ignore())
-            .ForMember(d => d.Childs, map => map.Ignore());
+
+
+            /*
+
+
+                       CreateMap<Tag, TagDto>();
+                       CreateMap<Category, CategoryDto>()
+                           .ForMember(d => d.Childs, opt => opt.Ignore());
+                       CreateMap<Comment, CommentDto>();
+
+                       CreateMap<Advertisement, AdvertisementDto>()
+                           .ForMember(d => d.Tags, opt => opt.Ignore());
+
+                       CreateMap<CommentDto, Comment>()
+                           .ForMember(d => d.CommentDate, opt => opt.Ignore());
+
+                       CreateMap<AdvertisementDto, Advertisement>()
+                           .ForMember(d => d.Id, opt => opt.Ignore())
+                           .ForMember(d => d.Tags, opt => opt.Ignore())
+                           .ForMember(d => d.Comments, opt => opt.Ignore())
+                           .ForMember(d => d.Category, opt => opt.Ignore());
+
+
+                       CreateMap<CategoryDto, Category>()
+                           .ForMember(d => d.Childs, opt => opt.Ignore());
+
+                       CreateMap<CategoryCreateDto, Category>()
+                       .ForMember(d => d.ParentCategory, map => map.Ignore())
+                       .ForMember(d => d.Childs, map => map.Ignore());  */
         }
     }
 }
--- WebApi/Mappings/ApiMappingsProfile.cs
+++ WebApi/Mappings/ApiMappingsProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using BusinessLogic.Services.Contracts.Models;
+using WebApi.Models.Advertisements;
+using WebApi.Models.Tags;
 using DataAccess.Entities;
 using System;
 using System.Collections.Generic;
@@ -9,23 +11,38 @@ using WebApi.Models;
 
 namespace WebApi.Mappings
 {
+    /// <summary>
+    /// Профиль автомаппера.
+    /// </summary>
     public class ApiMappingsProfile : Profile
     {
         public ApiMappingsProfile ()
         {
-            CreateMap<CategoryCreateModel, CategoryDto>()
-            .ForMember(d => d.ParentCategory, map => map.Ignore())
-            .ForMember(d => d.Childs, map => map.Ignore());
 
-            CreateMap<CategoryCreateModel, CategoryCreateDto>();
 
-            CreateMap<CategoryDto, CategoryGetModel>();
+            CreateMap<AdvertisementCreateModel, AdvertisementDto>()
+                .ForMember(d => d.Id, opt => opt.Ignore())
+                .ForMember(d => d.Comments, opt => opt.Ignore())
+                .ForMember(d => d.Category, opt => opt.Ignore());
+            CreateMap<TagModel, TagDto>();
 
-            CreateMap<CategoryDto, CategoryChildGetModel>();
 
-            CreateMap<CategoryUpdateModel, CategoryDto>()
-            .ForMember(d => d.ParentCategory, map => map.Ignore())
-            .ForMember(d => d.Childs, map => map.Ignore());
+
+
+
+            /* CreateMap<CategoryCreateModel, CategoryDto>()
+             .ForMember(d => d.ParentCategory, map => map.Ignore())
+             .ForMember(d => d.Childs, map => map.Ignore());
+
+             CreateMap<CategoryCreateModel, CategoryCreateDto>();
+
+             CreateMap<CategoryDto, CategoryGetModel>();
+
+             CreateMap<CategoryDto, CategoryChildGetModel>();
+
+             CreateMap<CategoryUpdateModel, CategoryDto>()
+             .ForMember(d => d.ParentCategory, map => map.Ignore())
+             .ForMember(d => d.Childs, map => map.Ignore());*/
         }
     }
 }
